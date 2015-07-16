3d88481f7c364fb03f4559a169fac2d5816c29c9
gravitano <gue.warsono@gmail.com>
2015-07-16 20:01:30 +0700

    PSR-2

--- Providers/UserServiceProvider.php
+++ Providers/UserServiceProvider.php
@@ -4,95 +4,95 @@ use Illuminate\Support\ServiceProvider;
 use Pingpong\Cms\User\Repositories\UserRepository;
 use Pingpong\Cms\User\Repositories\EloquentUserRepository;
 
-class UserServiceProvider extends ServiceProvider {
+class UserServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerConfig();
+        $this->registerTranslations();
+        $this->registerViews();
+        require __DIR__ . '/../menus.php';
+    }
 
-	/**
-	 * Boot the application events.
-	 * 
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->registerConfig();
-		$this->registerTranslations();
-		$this->registerViews();
-		require __DIR__ . '/../menus.php';
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(UserRepository::class, EloquentUserRepository::class);
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{		
-		$this->app->bind(UserRepository::class, EloquentUserRepository::class);
-	}
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            __DIR__ . '/../Config/config.php' => config_path('user.php'),
+        ]);
+        $this->mergeConfigFrom(
+            __DIR__ . '/../Config/config.php', 'user'
+        );
+    }
 
-	/**
-	 * Register config.
-	 * 
-	 * @return void
-	 */
-	protected function registerConfig()
-	{
-		$this->publishes([
-		    __DIR__.'/../Config/config.php' => config_path('user.php'),
-		]);
-		$this->mergeConfigFrom(
-		    __DIR__.'/../Config/config.php', 'user'
-		);
-	}
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = base_path('resources/views/modules/user');
 
-	/**
-	 * Register views.
-	 * 
-	 * @return void
-	 */
-	public function registerViews()
-	{
-		$viewPath = base_path('resources/views/modules/user');
+        $sourcePath = __DIR__ . '/../Resources/views';
 
-		$sourcePath = __DIR__.'/../Resources/views';
+        $this->publishes([
+            $sourcePath => $viewPath
+        ]);
 
-		$this->publishes([
-			$sourcePath => $viewPath
-		]);
+        $this->loadViewsFrom([$viewPath, $sourcePath], 'user');
+    }
 
-		$this->loadViewsFrom([$viewPath, $sourcePath], 'user');
-	}
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = base_path('resources/lang/modules/user');
 
-	/**
-	 * Register translations.
-	 * 
-	 * @return void
-	 */
-	public function registerTranslations()
-	{
-		$langPath = base_path('resources/lang/modules/user');
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'user');
+        } else {
+            $this->loadTranslationsFrom(__DIR__ . '/../Resources/lang', 'user');
+        }
+    }
 
-		if (is_dir($langPath)) {
-			$this->loadTranslationsFrom($langPath, 'user');
-		} else {
-			$this->loadTranslationsFrom(__DIR__ .'/../Resources/lang', 'user');
-		}
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
