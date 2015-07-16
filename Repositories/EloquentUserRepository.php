3ca67c5ad298fc1ca7db3e38c5bbe6411b556745
gravitano <gue.warsono@gmail.com>
2015-07-16 19:53:11 +0700

    remove useless use statement

--- Repositories/EloquentUserRepository.php
+++ Repositories/EloquentUserRepository.php
@@ -3,7 +3,6 @@
 namespace Pingpong\Cms\User\Repositories;
 
 use Pingpong\Cms\User\Entities\User;
-use Pingpong\Cms\User\Repositories\UserRepository;
 use Prettus\Repository\Eloquent\BaseRepository;
 
 class EloquentUserRepository extends BaseRepository implements UserRepository
