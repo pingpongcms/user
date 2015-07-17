2aa21180218ddd69e21b2609384e0dc1d4626b97
gravitano <gue.warsono@gmail.com>
2015-07-18 01:15:42 +0700

    make user model authenticable, auto hash password, add more fillable columns

--- Entities/User.php
+++ Entities/User.php
@@ -1,15 +1,36 @@
-<?php namespace Pingpong\Cms\User\Entities;
-   
+<?php
+
+namespace Pingpong\Cms\User\Entities;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model {
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+{
+    use Authenticatable, CanResetPassword;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-    	'name',
-    	'username',
-    	'email',
-    	'password',
-    	'confirmed'
+        'name', 'username', 'email', 'password',
+        'confirmation_code', 'confirmed'
     ];
 
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token', 'confirmation_code'];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
